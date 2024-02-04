7b6c593b646de0e0c1cd95fe5ee10ddaf244833a
Igor <trololol-lolo@mail.ru>
2024-02-04 11:18:37 +1000

    Добавляет компонент формы для отзывов

--- src/components/review-form/component.jsx
+++ src/components/review-form/component.jsx
@@ -1,21 +1,24 @@
+import classNames from "classnames";
 import { useReviewForm } from "./use-review-form";
+
+import styles from './styles.module.scss';
 
 
 export const ReviewForm = () => {
   const {form, setName, setText, setRating} = useReviewForm();
 
   return (
-    <div>
-      <div>
-        <label htmlFor="name">Name</label>
+    <div className={classNames(styles.root)}>
+      <div className={classNames(styles.field)}>
+        <label htmlFor="name">Имя</label>
         <input id="name" type="text" value={form.name} onChange={setName} />
       </div>
-      <div>
-        <label htmlFor="text">Text</label>
+      <div className={classNames(styles.field)}>
+        <label htmlFor="text">Текст</label>
         <input id="text" type="text" value={form.text} onChange={setText} />
       </div>
-      <div>
-        <label htmlFor="rating">Rating</label>
+      <div className={classNames(styles.field)}>
+        <label htmlFor="rating">Рейтинг</label>
         <input id="rating" type="number" min="0" max="5" value={form.rating} onChange={setRating} />
       </div>
     </div>
