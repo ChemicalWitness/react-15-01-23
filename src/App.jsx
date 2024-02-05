f71dc56f75b030277e075fe46975a528f0374b10
Igor <trololol-lolo@mail.ru>
2024-02-05 21:46:30 +1000

    Добавляет компонент login и его реализацию

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import { Layout } from "./components/layout/component";
 
 import './styles/index.scss';
 import { RestaurantPage } from "./pages/restaurant-page/component";
+import { UserContext } from "./context/user";
+import { useContext, useState } from "react";
 
 export const App = () => {
+  const [user, setUser] = useState('')
 
   return (
-    <Layout>
-      <RestaurantPage/>
-    </Layout>
+    <UserContext.Provider value={[user, setUser]}>
+      <Layout>
+        <RestaurantPage/>
+      </Layout>
+      </UserContext.Provider>
   );
 };
---
+++ src/components/login/component.jsx
@@ -0,0 +1,24 @@
+import { useContext, useState } from "react";
+import { Button } from "../button/component";
+import { UserContext } from "../../context/user";
+
+export const Login = () => {
+  const [user, setUser] = useContext(UserContext)
+  const [isLogin, setIsLogin] = useState(false)
+  const checkLogin = () => {
+    if (isLogin) {
+      setUser("Гость")
+      setIsLogin(false)
+    } else {
+      setUser({name: 'Adam', email: 'adGontier@gmail.com'})
+      setIsLogin(true)
+    }
+  }
+
+
+  return (
+    <div>
+      <Button onClick={checkLogin}>{isLogin ? 'Quit' : 'Log In'}</Button>
+    </div>
+  );
+};
