f552abf29040dce68ebc98e2cabb84e2361ef9b7
Igor <trololol-lolo@mail.ru>
2024-02-14 16:01:22 +1000

    Добавляет загрузку ресторанов с сервера

--- src/pages/restaurant-page/component.jsx
+++ src/pages/restaurant-page/component.jsx
@@ -1,9 +1,16 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import { RestaurantTabs } from "../../components/restaurant-tabs/component";
 import { Restaurant } from "../../components/restaurant/component";
+import { useDispatch } from "react-redux";
+import { getRestaurants } from "../../redux/entities/restaurant/thunk/get-restaurants";
 
 export const RestaurantPage = () => {
   const [activeRestaurantId, setActiveRestaurantId] = useState();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getRestaurants());
+  }, [activeRestaurantId])
 
   return (
     <div>
--- src/redux/index.js
+++ src/redux/index.js
@@ -3,10 +3,11 @@ import { restaurantSlice } from "./entities/restaurant";
 import { reviewSlice } from "./entities/review";
 import { dishSlice } from "./entities/dish";
 import { userSlice } from "./entities/user";
+import { requestSlice } from "./ui/request";
 
 
 export const store = configureStore({
-  reducer: combineSlices(restaurantSlice, reviewSlice, dishSlice, userSlice),
+  reducer: combineSlices(restaurantSlice, reviewSlice, dishSlice, userSlice, requestSlice),
 })
 
 console.log(store.getState())
